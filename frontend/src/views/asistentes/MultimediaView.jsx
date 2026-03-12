bf91ec234bf80f4ad9588024454c4207ee95eb0d
Ana Paula Barron Torres <ana.barron0974@alumnos.udg.mx>
2026-03-11 20:15:52 -0600

    CSS, ponencias (enviar, estatus, multimedia, extenso)
    integracioón de css, modificación de enviar ponencia, estatus ponencia. Pestañas de subir multimedia y extenso.

--- frontend/src/views/asistentes/MultimediaView.jsx
+++ frontend/src/views/asistentes/MultimediaView.jsx
@@ -1,9 +1,11 @@
 //subir multimedia de los ponentes, presentaciones, etc.
 import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export default function MultimediaView(){
+  const navigate=useNavigate();
   const[archivos, setArchivos]=useState([]);
-
+  //aqui se maneja la seleccion de archivos
   const handleFileChange=(e) => {
     const files = Array.from(e.target.files);
     //maximo de archivos que se pueden subir
@@ -11,23 +13,39 @@ export default function MultimediaView(){
       alert("Solo puedes subir 2 archivos.");
       return;
     }
+    //validar tamaño de los docs, 30 MB en total pendiente de ver
+    /* 
+    const maxTotalSize= 30*1024*1024;
+    const totalSize= file.reduce((acc, file)=> acc + file.size, 0);
+    if(totalSize > maxTotalSize){
+      alert("El tamaño no debe superar 30MB");
+      return;
+    }
+    setArchivos(files); //por el momento los archivos se guardan ahi hasta conectarlo con el back
+    */
   };
-
+  
+  //manejo del envio de archivos
   const handleSubmit = (e) => {
     e.preventDefault();
     if (archivos.length == 0) {
       alert("Debes seleccionar un archivo");
       return;
     }
-    console.log("Subir:", archivos);
-    alert("Se subieron correctamente");
+    console.log("Subir", archivos);
+    alert("Se subio correctamente");
   };
 
+
   return (
-    <div className="p-8 bg-white min-h-screen">
+    <div className="p-8 bg-base-100 min-h-screen">
+      {/*boton de regresar*/}
+      <button type="button" onClick={()=>navigate(-1)} className='btn btn-primary mb-4'>
+        ←
+      </button>
       <h1 className="text-2xl font-bold mb-6">Contenido Multimedia</h1>
-      <p className="mb-4 text-gray-700">
-        Añadir envío — Maximo 2 archivos.
+      <p className="mb-4 text-base-content">
+        Añadir envío, máximo 2 archivos.
       </p>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-xl">
@@ -36,18 +54,14 @@ export default function MultimediaView(){
           type="file"
           multiple
           onChange={handleFileChange}
-          className="border p-2"
+          className="file-input file-input-bordered w-full"
         />
 
-        {/*subir archivos*/}
-        <button
-          type="submit"
-          style={{ backgroundColor: "#001219", color: "white", padding: "8px 16px", borderRadius: "6px" }}
-        >
+        {/*boton de subir archivos*/}
+        <button type="submit" className="btn btn-primary">
           Subir
         </button>
       </form>
     </div>
   );
-
 }
