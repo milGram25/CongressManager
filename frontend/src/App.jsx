713590a524a942b7aa2c320c3559b943837dc767
Ana Paula Barron Torres <ana.barron0974@alumnos.udg.mx>
2026-03-13 18:19:44 -0600

    elimine multimedia y subir extenso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,6 @@ import PagosView from "./views/asistentes/PagosView";
 import MisPonenciasView from "./views/asistentes/MisPonenciasView";
 import EnviarPonenciaView from "./views/asistentes/EnviarPonenciaView";
 import EstatusPonenciaView from "./views/asistentes/EstatusPonenciaView";
-import MultimediaView from "./views/asistentes/MultimediaView";
-import SubirExtensoView from "./views/asistentes/SubirExtensoView";
 import Login from "./views/auth/Login";
 import Register from "./views/auth/Register";
 import RevisorLayout from "./views/revisores/layouts/MainLayout";
@@ -43,8 +41,6 @@ function App() {
             <Route path="mis-ponencias" element={<MisPonenciasView />} />
             <Route path="enviar-ponencia" element={<EnviarPonenciaView />} />
             <Route path="estatus-ponencia" element={<EstatusPonenciaView />} />
-            <Route path="subir-multimedia" element={<MultimediaView />} />
-            <Route path="subir-extenso" element={<SubirExtensoView />} />
           </Route>
 
           {/* Rutas para Revisores */}
