05dda04e6d9c264b953ef9129ff71d3e0752c21a
Ana Paula Barron Torres <ana.barron0974@alumnos.udg.mx>
2026-03-17 14:45:02 -0600

    Revert "Merge branch 'main' into asistente"
    This reverts commit ad95859accd034b56fa3da2051925e453f80c4f2, reversing
    changes made to 61dc7f9710971dbd3c777c1acc48715649cc2a2e.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
 import AsistenteLayout from "./views/asistentes/layouts/MainLayout";
 import AgendaView from "./views/asistentes/AgendaView";
 import CatalogoView from "./views/asistentes/CatalogoView";
@@ -8,28 +6,24 @@ import PagosView from "./views/asistentes/PagosView";
 import MisPonenciasView from "./views/asistentes/MisPonenciasView";
 import EnviarPonenciaView from "./views/asistentes/EnviarPonenciaView";
 import EstatusPonenciaView from "./views/asistentes/EstatusPonenciaView";
-import MultimediaView from "./views/asistentes/MultimediaView";
-import SubirExtensoView from "./views/asistentes/SubirExtensoView";
-import ConstanciasView from "./views/asistentes/ConstanciasView";
 import Login from "./views/auth/Login";
 import Register from "./views/auth/Register";
 import RevisorLayout from "./views/revisores/layouts/MainLayout";
 import RevisionesView from "./views/revisores/RevisionesView";
 import PlaceholderView from "./views/revisores/PlaceholderView";
 import DetalleRevisionView from "./views/revisores/DetalleRevisionView";
-import DictaminadorLayout from "./views/dictaminadores/layouts/MainLayout";
-import DetalleDictamenView from "./views/dictaminadores/DetalleDictamenView";
-import DictamenesView from "./views/dictaminadores/DictamenesView";
 
 // Routea a las diferentes vistas del sistema
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Ruta pública: login y registro */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    // Definicion de rutas
+    <BrowserRouter>
+      {/* Rutas "root" ej. https:asdf.com/{root} */}
+      <Routes>
+        {/* Rutas sub asistente ej. https:asdf.com/asistente/{subruta} */}
+        {/* Estas rutasa se encutran dentro de la carpeta views/asistentes */}
+        <Route path="/asistente" element={<AsistenteLayout />}>
+          <Route index element={<Navigate to="agenda" replace />} />
 
           <Route path="agenda" element={<AgendaView />} />
           <Route path="catalogo" element={<CatalogoView />} />
@@ -54,50 +48,13 @@ function App() {
             <Route path="mis-ponencias" element={<MisPonenciasView />} />
             <Route path="enviar-ponencia" element={<EnviarPonenciaView />} />
             <Route path="estatus-ponencia" element={<EstatusPonenciaView />} />
-            <Route path="subir-multimedia" element={<MultimediaView />} />
-            <Route path="subir-extenso" element={<SubirExtensoView />} />
-            <Route path="constancias" element={<ConstanciasView title="Mis Constancias" />} />
           </Route>
 
-          {/* Rutas para Revisores */}
-          <Route
-            path="/revisor"
-            element={
-              <ProtectedRoute allowedRole="revisor">
-                <RevisorLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate to="revisiones" replace />} />
-            <Route path="inicio" element={<PlaceholderView title="Inicio" />} />
-            <Route path="revisiones" element={<RevisionesView />} />
-            <Route path="revision/:id" element={<DetalleRevisionView />} />
-            <Route path="historial" element={<PlaceholderView title="Historial" />} />
-          </Route>
-
-          {/* Rutas para Dictaminadores */}
-          <Route
-            path="/dictaminador"
-            element={
-              <ProtectedRoute allowedRole="dictaminador">
-                <DictaminadorLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate to="dictamenes" replace />} />
-            <Route path="inicio" element={<PlaceholderView title="Inicio" />} />
-            <Route path="dictamenes" element={<DictamenesView />} />
-            <Route path="dictamen/:id" element={<DetalleDictamenView />} />
-            <Route path="historial" element={<PlaceholderView title="Historial" />} />
-          </Route>
-
-          {/* Por defecto va al login */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+        {/* Por defecto va a asistente hasta que tengamos algo mas */}
+        <Route path="*" element={<Navigate to="/asistente/catalogo" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
