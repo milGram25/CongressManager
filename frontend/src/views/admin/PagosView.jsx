23212b30109bc28c1e59e8565ffe656f747683c9
ErnestoGtz <ernesto.gutierrez9298@alumnos.udg.mx>
2026-03-28 13:16:57 -0600

    cambio de PagosComponente implementado en la vista

--- frontend/src/views/admin/PagosView.jsx
+++ frontend/src/views/admin/PagosView.jsx
@@ -1,8 +1,18 @@
+import PagosComponente from "./Componentes/PagosComponente"
+
 export default function AdminPagosView() {
   return (
-    <div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px] flex flex-col items-center justify-center text-center">
+    
+    
+    <div>
+      {/*<div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px] flex flex-col items-center justify-center text-center">
       <h2 className="text-2xl font-bold">Pagos</h2>
       <p className="text-base-content/50 mt-2 italic">Sección en proceso de desarrollo...</p>
-    </div>
+      
+      
+    </div>*/}
+      <PagosComponente/>
+      
+      </div>
   );
 }
