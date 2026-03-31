062b8e7a8787f6a9cd1051a0b8eafa4fde375dab
ErnestoGtz <ernesto.gutierrez9298@alumnos.udg.mx>
2026-03-31 12:52:45 -0600

    Agregué la lista de elementos genérica y lo integré en eventos > congresos > detalles

--- frontend/src/views/admin/ProcesosResumenesView.jsx
+++ frontend/src/views/admin/ProcesosResumenesView.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
+import ListaElementosGenerica from "./Componentes/ListaElementosGenericaComponente";
 
 export default function ProcesosResumenesView() {
   const navigate = useNavigate();
   return (
-    <div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px]">
+    <div>
+      <ListaElementosGenerica/>
+      {/*<div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px]">
       <button 
         onClick={() => navigate(-1)} 
         className="mb-6 p-2 hover:bg-base-200 rounded-full transition-colors flex items-center gap-2 text-base-content/70 hover:text-primary"
@@ -17,6 +20,10 @@ export default function ProcesosResumenesView() {
         <h2 className="text-2xl font-bold">Procesos: Resúmenes</h2>
         <p className="text-base-content/50 mt-2 italic">Sección en proceso de desarrollo...</p>
       </div>
+    </div>*/}
+
+
     </div>
+    
   );
 }
