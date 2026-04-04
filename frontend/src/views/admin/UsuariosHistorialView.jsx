01cf3fa29e4720e859502db1575a964d04beccde
ErnestoGtz <ernesto.gutierrez9298@alumnos.udg.mx>
2026-04-04 12:50:24 -0600

    Creé los componentes de las listas, mejoré el componente de ListaElementosCongresosComponente, ya hice funcionar al componente de ListaHistorial, y ya integré los cambios de estos últimos dos componentes a sus respectivos frames

--- frontend/src/views/admin/UsuariosHistorialView.jsx
+++ frontend/src/views/admin/UsuariosHistorialView.jsx
@@ -1,22 +1,48 @@
 import { useNavigate } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
+import ListaHistorial from "./Componentes/ListaHistorial";
 
 export default function UsuariosHistorialView() {
   const navigate = useNavigate();
+
+  const lista = [
+        {
+            id:1,
+            nombre: "Ernesto",
+            fecha: "2025-03-30 23:59:59.00000",
+            rol: "comite academico",
+            accion: "modificar fecha evento"
+        },
+        {
+            id:2,
+            nombre: "Jimenita",
+            fecha: "2026-04-04 10:59:59.00000",
+            rol: "ponente",
+            accion: "realizar pago"
+
+        },
+        {
+            id:3,
+            nombre: "Kaleb",
+            fecha: "2026-03-15 23:59:59.00000",
+            rol: "evaluador",
+            accion: "cerrar sesion"
+
+        },
+        {
+            id:3,
+            nombre: "Forense",
+            fecha: "2026-03-15 23:59:59.00000",
+            rol: "dictaminador",
+            accion: "solicitar ponencia"
+
+        }
+    ];
+
   return (
     <div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px]">
-      <button 
-        onClick={() => navigate(-1)} 
-        className="mb-6 p-2 hover:bg-base-200 rounded-full transition-colors flex items-center gap-2 text-base-content/70 hover:text-primary"
-        title="Regresar a la vista anterior"
-      >
-        <MdArrowBack className="text-2xl" />
-        <span className="text-sm font-medium">Regresar</span>
-      </button>
-      <div className="flex flex-col items-center justify-center py-20 text-center">
-        <h2 className="text-2xl font-bold">Usuarios: historial</h2>
-        <p className="text-base-content/50 mt-2 italic">Sección en proceso de desarrollo...</p>
-      </div>
+      <ListaHistorial listaElementos={lista}/>
+     
     </div>
   );
 }
