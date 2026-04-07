19ea02ee4fed6537091c64225279142888dd1ce3
ErnestoGtz <ernesto.gutierrez9298@alumnos.udg.mx>
2026-04-07 11:01:20 -0600

    Agregué un pequeño texto

--- frontend/src/views/admin/UsuariosFacturasView.jsx
+++ frontend/src/views/admin/UsuariosFacturasView.jsx
@@ -6,6 +6,15 @@ export default function UsuariosFacturasView() {
   const navigate = useNavigate();
   return (
     <div className="bg-base-100 p-8 rounded-3xl border border-base-300 shadow-sm min-h-[400px]">
+
+      <div className="flex gap-4">
+        <div className="border bg-black rounded-full h-10 w-2"></div>
+        <h2 className="flex-1 text-2xl font-bold text-start">Adjuntar factura</h2>
+      </div>
+      <p className="pt-2 pl-12 text-start mb-4">
+        Selecciona a la persona y gestiona las facturas según sus roles de este congreso
+      </p>
+
       <AdjuntarFactura/>
 
     </div>
