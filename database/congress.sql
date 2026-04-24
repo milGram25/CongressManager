c8cd817e46b6e05b0273158b01b4f8426c838df2
ana.barron0974@alumnos.udg.mx <ana.barron0974@alumnos.udg.mx>
2026-04-24 12:53:32 -0600

    Back de enviar ponencia, agregue campos a la base de datos en resumen y extenso

--- database/congress.sql
+++ database/congress.sql
@@ -211,6 +211,9 @@ CREATE TABLE resumen (
     revisado BOOLEAN DEFAULT FALSE,
     estatus estatus_resumen_enum,
     retroalimentacion TEXT
+    titulo VARCHAR(255),
+    palabras_clave VARCHAR(255),
+    contenido TEXT
 );
 
 CREATE TABLE extenso (
@@ -218,7 +221,8 @@ CREATE TABLE extenso (
     titulo VARCHAR(255) NOT NULL,
     fecha_subida TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     revisado BOOLEAN DEFAULT FALSE,
-    version_numero INTEGER DEFAULT 1 -- Para manejar "Solicitud de cambios"
+    version_numero INTEGER DEFAULT, 1 -- Para manejar "Solicitud de cambios"
+    ruta_archivo VARCHAR(255)
 );
 
 CREATE TABLE ponencia (
@@ -228,7 +232,10 @@ CREATE TABLE ponencia (
     id_subarea INTEGER NOT NULL REFERENCES subareas(id_subareas),
     id_resumen INTEGER NOT NULL REFERENCES resumen(id_resumen),
     id_extenso INTEGER REFERENCES extenso(id_extenso),
-    id_multimedia INTEGER REFERENCES multimedia(id_material)
+    id_multimedia INTEGER REFERENCES multimedia(id_material),
+    autor_principal VARCHAR(255), 
+    coautores TEXT,
+    id_tipo_trabajo INTEGER REFERENCES tipo_trabajo(id_tipo_trabajo)
 );
 
 -- Te parece bien asi la tabla de taller?
