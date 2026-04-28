a5b30c5d8dab152bc675aaec1e90a419f1510d69
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2026-04-28 08:22:17 -0600

    fix - importación de EnviarPonenciaApiView

--- backend/ponencias/urls.py
+++ backend/ponencias/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import registrar_ponencia, listar_catalogo_ponencias, PonenciaViewSet
+from .views import registrar_ponencia, listar_catalogo_ponencias, PonenciaViewSet, EnviarPonenciaAPIView
 
 app_name = 'ponencias'
 
