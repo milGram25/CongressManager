626c070d09ae7a759fa59348ff42d26c9c112730
Diego Muñoz Cisneros <diegomu035@gmail.com>
2026-05-17 22:55:08 -0600

    feat: agregar campo ruta_xml a modelo Factura

--- backend/users/models.py
+++ backend/users/models.py
@@ -88,6 +88,7 @@ class Factura(models.Model):
     regimen_fiscal = models.CharField(max_length=255, blank=True, null=True)
     ruta_pdf_xml = models.CharField(max_length=255, blank=True, null=True)
     ruta_constancia_fiscal = models.CharField(max_length=255, blank=True, null=True)
+    ruta_xml = models.CharField(max_length=255, blank=True, null=True)
     estatus = models.CharField(max_length=20, default='pendiente')
     fecha_solicitud = models.DateTimeField(auto_now_add=True)
     fecha_envio = models.DateTimeField(blank=True, null=True)
